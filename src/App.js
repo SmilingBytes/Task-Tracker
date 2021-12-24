77d613853668a04ca736e23a7c0a4b92c5272cea
Ismail H Rana <ismail.kuet@gmail.com>
2021-12-24 18:21:42 +0600

    local update done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './App.css';
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 const App = () => {
@@ -18,6 +19,7 @@ const App = () => {
   };
 
 
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id: 1,
@@ -39,10 +41,16 @@ const App = () => {
     },
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
-    console.log("Delete", id)
   }
 
   //Toggle Reminder
@@ -53,7 +61,8 @@ const App = () => {
 
   return (
     <div className='container'>
-      <Header title='Task Tracker' />
+      <Header title='Task Tracker' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
+      { showAddTask && <AddTask onAdd={ addTask } />}
       { tasks.length > 0 ? 
         <Tasks tasks={ tasks } onDelete={deleteTask} onToggle={toggleReminder} /> : 
         'No Task To Show'}
